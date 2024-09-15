8b0ae459ecb8c1533829d6bfff26120dd926a408
rashilmaharjan <rashilmaharjan25@gmail.com>
2024-09-15 19:01:46 +1000

    setting up and builiding project/display items in the list/adding key to array elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+
+  const handleOnChange = (e) => {
+    //Getting real-time typing Display
+    //synthetic base event
+    const { value } = e.target; //destructuring
+    setName(value); //executing function
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, name]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div
+        className="wrapper"
+        style={{
+          display: "flex",
+          height: "100vh",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "2rem",
+        }}
+      >
+        <div
+          className="userList"
+          style={{ boxShadow: "0 0 10px grey", padding: "2rem" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="display">{name}</div>
+          <div className="form">
+            <form action="" onSubmit={handleOnSubmit}>
+              <input type="text" onChange={handleOnChange} />
+              <button>Add User</button>
+            </form>
+            <hr />
+          </div>
+          <div className="list">
+            <ul>
+              {list.map((item, i) => {
+                //{list.map((item, i) => (<li key={i}>{item}</li>))} //different method
+                return <li key={i}>{item}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
